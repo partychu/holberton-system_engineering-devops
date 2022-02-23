a4a908365ddb4e9fd4a0b6be828836d9b4c01b90
partychu <ytorres667@gmail.com>
2022-02-23 19:35:51 +0000

    Checking 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,20 +1,20 @@
 #!/usr/bin/python3
 """
-Queries Reddit API and returns # of subs
+Queries Reddit API and returns top 10 posts
 """
 
 import requests
 
 
 def top_ten(subreddit):
-    """ Returns number of subscribers """
+    """ Returns top 10 posts """
     headers = {'User-Agent': 'Mozilla/5.0)'}
     url = 'http://www.reddit.com/r/{}/top.json?limit=10'.format(subreddit)
     r = requests.get(url, headers=headers)
-
     if r.status_code != 200:
-        return 0
+        print(None)
+    if r.json()['data']['after'] == None:
+        print(None)
     else:
         for post in r.json()['data']['children']:
-            x = post['data']['title']
-            print(x)
+            print(post['data']['title'])
