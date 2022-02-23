c1acb3b1ed8f5be6c787b35bb964431385638f79
partychu <ytorres667@gmail.com>
2022-02-23 19:55:25 +0000

    Checking 1

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -9,12 +9,16 @@ import requests
 def top_ten(subreddit):
     """ Returns top 10 posts """
     headers = {'User-Agent': 'Mozilla/5.0)'}
-    url = 'http://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
-    r = requests.get(url, headers=headers)
+    url = 'https://www.reddit.com/r/{}/hot.json?limit=10'.format(subreddit)
+
+    r = requests.get(url, headers=headers, allow_redirects=False)
+
     if r.status_code != 200:
         print(None)
-    if r.json()['data']['after'] is None:
-        print(None)
-    else:
-        for post in r.json()['data']['children']:
+        return
+    try:
+        posts = r.json()['data']['children']
+        for post in posts:
             print(post['data']['title'])
+    except:
+        print(None)
