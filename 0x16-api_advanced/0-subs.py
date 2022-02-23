585922faf16203da13608a01bd7c5c3784957a04
partychu <ytorres667@gmail.com>
2022-02-23 18:45:39 +0000

    Add header

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -8,7 +8,11 @@ import requests
 
 def number_of_subscribers(subreddit):
     """ Returns number of subscribers """
-    r = requests.get('http://www.reddit.com/r/{}/about.json'.format(subreddit))
+    headers = {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:97.0)'
+    }
+    url = 'http://www.reddit.com/r/{}/about.json'.format(subreddit)
+    r = requests.get(url, headers=headers)
 
     if r.status_code != 200:
         return 0
